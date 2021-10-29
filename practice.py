ef6a5e823b0d04ffebe2337e3e9e247630ddd839
losmang <c.mendez1@yahoo.com>
2021-10-28 21:29:03 -0400

    review stuff again...easy fly though. review section 6 next

--- practice.py
+++ practice.py
@@ -1,3 +1,10 @@
-num = int(input("Enter a number here: "))
+age = int(input("How old are you? "))
 
-print("You chose:",num)
+if (age <= 5):
+    print("You are to young for schoool...Go out and play!")
+elif (age == 6):
+    print("Go to kindergarten")
+elif (age > 6) and (age < 19):
+    print("You should be in grade {}".format(age - 6))
+else:
+    print("Go to college or get a job or something")
