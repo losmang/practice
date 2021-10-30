9baa1152a778e9c1c5e0c1ea847a0a890deebdb5
losmang <c.mendez1@yahoo.com>
2021-10-29 20:06:52 -0400

    Do chapter 6 next

--- practice.py
+++ practice.py
@@ -1,10 +1,1 @@
-age = int(input("How old are you? "))
-
-if (age <= 5):
-    print("You are to young for schoool...Go out and play!")
-elif (age == 6):
-    print("Go to kindergarten")
-elif (age > 6) and (age < 19):
-    print("You should be in grade {}".format(age - 6))
-else:
-    print("Go to college or get a job or something")
+print("New version")
