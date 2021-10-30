393854d8f92b94813bde55f4294fb9e1f3462d0e
losmang <c.mendez1@yahoo.com>
2021-10-30 00:05:29 -0400

    Re-review chapeter 6 - 10

--- practice.py
+++ practice.py
@@ -1,2 +1,21 @@
-print("New version")
-print("Test version")
+treeRows = int(input("Enter how many rows your tree should be: "))
+
+spaces = treeRows -1
+hashes = 1
+stump = treeRows -1
+
+while treeRows != 0:
+    for i in range(spaces):
+        print(' ',end = '')
+    for i in range(hashes):
+        print('#', end = '')
+    print()
+
+    spaces -= 1
+    hashes += 2
+    treeRows -= 1
+
+for i in range(stump):
+    print(' ', end = '')
+
+print('#')
